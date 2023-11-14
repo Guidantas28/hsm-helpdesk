36252f1896e485d3dea492a3bfbfc7f991cea45c
Guilherme <guilhermedantas788@gmail.com>
2023-11-14 13:09:11 -0300

    started chamados config from mongodb

---
+++ src/models/Chamado.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const ChamadoSchema = new mongoose.Schema({
+    titulo: {type: String, required: true},
+    descricao: {type: String, required: true},
+    status: {type: String, enum: ['Aberto', 'Em andamento', 'Fechado'], default: 'Aberto', required: true},	
+    dataAbertura: {type: Date, default: Date.now},
+    dataFechamento: {type: Date},
+    usuario: {type: mongoose.Schema.Types.ObjectId, ref: 'name', required: true},
+});
+
+const Chamado = mongoose.model('Chamado', ChamadoSchema);
+
+export default Chamado;
